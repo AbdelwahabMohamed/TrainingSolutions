ee5b6b48c16c8513e6df5b6204eadfe6e283f546
AbdelwahabMohamed <AbdelwahabSadek@gmail.com>
2017-02-20 20:08:49 +0100

    Initial Entity Creator
    Built on Spackle to generate random complex objects

---
+++ EntityCreator/EntityCreator.Tests/EntityCreatorTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EntityCreator;
+using System;
+using System.Diagnostics;
+
+namespace EntityCreator.Tests
+{
+    [TestClass]
+    public class EntityCreatorTests
+    {
+
+        [TestMethod]
+        public void Primitive_Initialized()
+        {
+            var created = EntityCreator.Create<int>();
+            Assert.IsInstanceOfType(created, typeof(int));
+        }
+
+        [TestMethod]
+        public void Complex_Initialized()
+        {
+            var createdComplex = EntityCreator.Create<ComplexType>();
+            Assert.IsInstanceOfType(createdComplex.DateTime, typeof(DateTime));
+            Assert.IsNotNull(createdComplex.DateTime);
+            Assert.IsTrue(createdComplex.Number != 0);            
+        }
+    }
+
+    public class ComplexType
+    {
+        public int Number { get; set; }
+        public string String { get; set; }
+        public DateTime DateTime { get; set; }
+    }
+}
---
+++ EntityCreator/EntityCreator/EntityCreator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using Spackle;
+
+namespace EntityCreator
+{
+    public static class EntityCreator
+    {
+        public static T Create<T>()
+            where T : new()
+        {
+            var generator = new RandomObjectGenerator();
+            var entity = new T();
+            var properties = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance).Where(_ => _.CanWrite).ToList();
+            properties.ForEach(_ =>
+            {
+                try
+                {
+                    _.SetValue(entity,
+                        typeof(RandomObjectGenerator)
+                            .GetMethod("Generate", Type.EmptyTypes)
+                            .MakeGenericMethod(new[] { _.PropertyType })
+                            .Invoke(generator, null));
+                }
+                catch (TargetInvocationException)
+                {
+                }
+            });
+            return entity;
+        }
+
+        public static T Create<T>(Action<T> modifier)
+            where T : new()
+        {
+            var entity = Create<T>();
+            modifier(entity);
+            return entity;
+        }
+    }
+}
